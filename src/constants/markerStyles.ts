003e62b237439bdacf161d0c7af9fd9b2586a390
florianleucht <fsleucht@stud.ntnu.no>
2025-04-30 11:43:52 +0200

    fix: changed name

--- src/constants/markerStyles.ts
+++ src/constants/markerStyles.ts
@@ -2,8 +2,8 @@ export const MARKER_COLORS = {
   hospital: '#ff4d4d',      // Red
   shelter: '#4da6ff',       // Blue
   defibrillator: '#ffcc00', // Yellow
-  waterStation: '#00ccff',  // Light blue
-  foodCentral: '#66cc66',   // Green
+  water_station: '#00ccff',  // Light blue
+  food_central: '#66cc66',   // Green
   default: '#808080'        // Gray
 };
 
@@ -11,7 +11,7 @@ export const MARKER_LABELS = {
   hospital: 'H',
   shelter: 'S',
   defibrillator: 'D',
-  waterStation: 'W',
-  foodCentral: 'F',
+  water_station: 'W',
+  food_central: 'F',
   default: '?'
 };

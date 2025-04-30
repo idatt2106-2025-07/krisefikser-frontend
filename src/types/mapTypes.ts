51f209d8c6155fd9049ebbdb13497af7919fbf93
florianleucht <fsleucht@stud.ntnu.no>
2025-04-30 09:29:27 +0200

    feat: updated naming in mapTypes

--- src/types/mapTypes.ts
+++ src/types/mapTypes.ts
@@ -1,36 +1,40 @@
 export interface LocationItem {
-  id: string;
-  name: string;
-  coordinates: [number, number];
-  address?: string;
+  id: string
+  name: string
+  coordinates: [number, number]
+  address?: string
 }
 
 export interface AffectedArea extends LocationItem {
-  radius: number;
+  radius: number
 }
 
 export interface LocationData {
-  hospitals: LocationItem[];
-  shelters: LocationItem[];
-  defibrillators: LocationItem[];
-  waterStations: LocationItem[];
-  foodCentrals: LocationItem[];
-  affectedAreas: AffectedArea[];
+  hospitals: LocationItem[]
+  shelters: LocationItem[]
+  defibrillators: LocationItem[]
+  water_stations: LocationItem[]
+  food_centrals: LocationItem[]
+  affected_areas: AffectedArea[]
 }
 
 export interface MarkerCollections {
-  hospitals: mapboxgl.Marker[];
-  shelters: mapboxgl.Marker[];
-  defibrillators: mapboxgl.Marker[];
-  waterStations: mapboxgl.Marker[];
-  foodCentrals: mapboxgl.Marker[];
+  hospitals: mapboxgl.Marker[]
+  shelters: mapboxgl.Marker[]
+  defibrillators: mapboxgl.Marker[]
+  water_stations: mapboxgl.Marker[]
+  food_centrals: mapboxgl.Marker[]
 }
 
 export interface Filters {
-  hospital?: boolean;
-  shelter?: boolean;
-  defibrillator?: boolean;
-  waterStation?: boolean;
-  foodCentral?: boolean;
-  affectedAreas?: boolean;
+  hospital?: boolean
+  shelter?: boolean
+  defibrillator?: boolean
+  water_station?: boolean
+  food_central?: boolean
+  affected_areas?: boolean
+}
+
+export interface GetPointsOfInterestRequest {
+  types: string[]
 }

5f79e41078705786e01d9b513657545e0c53f72c
florianleucht <fsleucht@stud.ntnu.no>
2025-05-02 11:43:25 +0200

    feat: added method to get all POIs to mapService

--- src/services/mapService.ts
+++ src/services/mapService.ts
@@ -18,6 +18,17 @@ class MapService {
     const response = await axiosInstance.get('/affected-area')
     return response.data
   }
+
+  async getAllPointsOfInterest() {
+    const allTypes = [
+      'hospital', 'shelter', 'defibrillator',
+      'water_station', 'food_central', 'meeting_place'
+    ];
+    const response = await axiosInstance.get('/point-of-interest', {
+      params: { types: allTypes.join(',') }
+    });
+    return response.data;
+  }
 }
 
 const mapService = new MapService()

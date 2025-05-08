d197ef8484cd876ab57f77572e9280c6faef8b7d
emilruud <emilruud@ntnu.no>
2025-05-08 20:02:26 +0200

    feat: enhance HouseholdService with new methods for household management

--- src/services/HouseholdService.ts
+++ src/services/HouseholdService.ts
@@ -9,7 +9,50 @@ class HouseholdService {
    * @returns A promise that resolves to the household details.
    */
   async getMyHouseholdDetails() {
-    const response = await axiosInstance.get('/household')
+    const response = await axiosInstance.get('/households')
+    return response.data
+  }
+
+  async leaveAndCreateHousehold(householdRequest: { name: string; longitude: number; latitude: number }) {
+    const response = await axiosInstance.post('/households', householdRequest)
+    return response.data
+  }
+
+  async requestToJoinHousehold(householdId: number) {
+    const response = await axiosInstance.post('/households/join-request', {
+      householdId,
+    })
+    return response.data
+  }
+
+  async getJoinRequests() {
+    const response = await axiosInstance.get('/households/requests')
+    return response.data
+  }
+
+
+  async acceptJoinRequest(requestId: number) {
+    await axiosInstance.put(`/households/requests/${requestId}/accept`)
+  }
+
+  async declineJoinRequest(requestId: number) {
+    await axiosInstance.put(`/households/requests/${requestId}/decline`)
+  }
+
+  async createInvitation(email: string) {
+    const response = await axiosInstance.post('/household-invitations', { email })
+    return response.data
+  }
+
+  async verifyInvitation(token: string) {
+    const response = await axiosInstance.get('/household-invitations/verify', {
+      params: { token }
+    })
+    return response.data
+  }
+
+  async acceptInvitation(token: string) {
+    const response = await axiosInstance.post('/household-invitations/accept', { token })
     return response.data
   }
 
